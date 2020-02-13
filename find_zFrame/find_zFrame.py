03aa93b2eea854f5ba79dd9f4199c2c763b57ee2
dorian.vogel <dorian.vogel@fhnw.ch>
2020-02-13 16:25:06 +0100

    Initial commit, this integrates the segmentZframe script into an extension.

---
+++ find_zFrame/find_zFrame.py
@@ -0,0 +1,245 @@
+import os
+import unittest
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+import logging
+
+#
+# find_zFrame
+#
+
+class find_zFrame(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "find_zFrame" # TODO make this more human readable by adding spaces
+    self.parent.categories = ["Examples"]
+    self.parent.dependencies = []
+    self.parent.contributors = ["John Doe (AnyWare Corp.)"] # replace with "Firstname Lastname (Organization)"
+    self.parent.helpText = """
+This is an example of scripted loadable module bundled in an extension.
+It performs a simple thresholding on the input volume and optionally captures a screenshot.
+"""
+    self.parent.helpText += self.getDefaultModuleDocumentationLink()
+    self.parent.acknowledgementText = """
+This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+""" # replace with organization, grant and thanks.
+
+#
+# find_zFrameWidget
+#
+
+class find_zFrameWidget(ScriptedLoadableModuleWidget):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setup(self):
+    ScriptedLoadableModuleWidget.setup(self)
+
+    # Instantiate and connect widgets ...
+
+    #
+    # Parameters Area
+    #
+    parametersCollapsibleButton = ctk.ctkCollapsibleButton()
+    parametersCollapsibleButton.text = "Parameters"
+    self.layout.addWidget(parametersCollapsibleButton)
+
+    # Layout within the dummy collapsible button
+    parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
+
+    #
+    # input volume selector
+    #
+    self.inputSelector = slicer.qMRMLNodeComboBox()
+    self.inputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.inputSelector.selectNodeUponCreation = True
+    self.inputSelector.addEnabled = False
+    self.inputSelector.removeEnabled = False
+    self.inputSelector.noneEnabled = False
+    self.inputSelector.showHidden = False
+    self.inputSelector.showChildNodeTypes = False
+    self.inputSelector.setMRMLScene( slicer.mrmlScene )
+    self.inputSelector.setToolTip( "Pick the input to the algorithm." )
+    parametersFormLayout.addRow("Input Volume: ", self.inputSelector)
+
+    #
+    # output volume selector
+    #
+    self.outputSelector = slicer.qMRMLNodeComboBox()
+    self.outputSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.outputSelector.selectNodeUponCreation = True
+    self.outputSelector.addEnabled = True
+    self.outputSelector.removeEnabled = True
+    self.outputSelector.noneEnabled = True
+    self.outputSelector.showHidden = False
+    self.outputSelector.showChildNodeTypes = False
+    self.outputSelector.setMRMLScene( slicer.mrmlScene )
+    self.outputSelector.setToolTip( "Pick the output to the algorithm." )
+    parametersFormLayout.addRow("Output Segmentation: ", self.outputSelector)
+
+    #
+    # Image type radios
+    #
+    self.imgType='T1'
+    self.imgTypeHBox = qt.QHBoxLayout()
+    self.t1TypRadio =  qt.QRadioButton("T1")
+    self.ctTypRadio =  qt.QRadioButton("CT")
+    self.t1TypRadio.setChecked(True)
+    self.ctTypRadio.setChecked(False)
+    self.imgTypeHBox.addWidget(self.t1TypRadio)
+    self.imgTypeHBox.addWidget(self.ctTypRadio)
+    parametersFormLayout.addRow("Image modality: ", self.imgTypeHBox)
+    
+    
+    
+    #
+    # Apply Button
+    #
+    self.applyButton = qt.QPushButton("Apply")
+    self.applyButton.toolTip = "Run the algorithm."
+    self.applyButton.enabled = False
+    parametersFormLayout.addRow(self.applyButton)
+
+    # connections
+    self.applyButton.connect('clicked(bool)', self.onApplyButton)
+    self.inputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    self.outputSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onSelect)
+    self.t1TypRadio.connect('toggled(bool)', self.ont1TypeToggle)
+    self.ctTypRadio.connect('toggled(bool)', self.onctTypeToggle)
+    
+    
+    # Add vertical spacer
+    self.layout.addStretch(1)
+
+    # Refresh Apply button state
+    self.onSelect()
+
+  def cleanup(self):
+    pass
+
+  def onSelect(self):
+    self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode()
+  
+  def ont1TypeToggle(self, checked):
+      if checked: self.imgType = 'T1'
+      #print("imgType: %s"%self.imgType)
+  def onctTypeToggle(self, checked):
+      if checked: self.imgType = 'CT'
+      #print("imgType: %s"%self.imgType)
+  
+  def onApplyButton(self):
+    logic = find_zFrameLogic()
+    logic.run(self.inputSelector.currentNode(), self.outputSelector.currentNode(), self.imgType)
+
+#
+# find_zFrameLogic
+#
+
+class find_zFrameLogic(ScriptedLoadableModuleLogic):
+  """This class should implement all the actual
+  computation done by your module.  The interface
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget.
+  Uses ScriptedLoadableModuleLogic base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def hasImageData(self,volumeNode):
+    """This is an example logic method that
+    returns true if the passed in volume
+    node has valid image data
+    """
+    if not volumeNode:
+      logging.debug('hasImageData failed: no volume node')
+      return False
+    if volumeNode.GetImageData() is None:
+      logging.debug('hasImageData failed: no image data in volume node')
+      return False
+    return True
+
+  def isValidInputOutputData(self, inputVolumeNode, outputLabelMapNode):
+    """Validates if the output is not the same as input
+    """
+    if not inputVolumeNode:
+      logging.debug('isValidInputOutputData failed: no input volume node defined')
+      return False
+    if not outputLabelMapNode:
+      logging.debug('isValidInputOutputData failed: no output volume node defined')
+      return False
+    if inputVolumeNode.GetID()==outputLabelMapNode.GetID():
+      logging.debug('isValidInputOutputData failed: input and output volume is the same. Create a new volume for output to avoid this error.')
+      return False
+    return True
+
+  def run(self, inputVolume, outputVolume, imgType):
+    """
+    Run the actual algorithm
+    """
+
+    if not self.isValidInputOutputData(inputVolume, outputVolume):
+      slicer.util.errorDisplay('Input volume is the same as output volume. Choose a different output volume.')
+      return False
+
+    logging.info('Processing started')
+
+    # Compute the thresholded output volume using the Threshold Scalar Volume CLI module
+    import segmentZframe
+    segmentZframe.segment_zFrame_slicer(inputVolume, outputVolume, imgType)
+
+
+    logging.info('Processing completed')
+
+    return True
+
+class find_zFrameTest(ScriptedLoadableModuleTest):
+  """
+  This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_find_zFrame1()
+
+  def test_find_zFrame1(self):
+    """ Ideally you should have several levels of tests.  At the lowest level
+    tests should exercise the functionality of the logic with different inputs
+    (both valid and invalid).  At higher levels your tests should emulate the
+    way the user would interact with your code and confirm that it still works
+    the way you intended.
+    One of the most important features of the tests is that it should alert other
+    developers when their changes will have an impact on the behavior of your
+    module.  For example, if a developer removes a feature that you depend on,
+    your test should break so they know that the feature is needed.
+    """
+
+    self.delayDisplay("Starting the test")
+    #
+    # first, get some data
+    #
+    import SampleData
+    SampleData.downloadFromURL(
+      nodeNames='FA',
+      fileNames='FA.nrrd',
+      uris='http://slicer.kitware.com/midas3/download?items=5767')
+    self.delayDisplay('Finished with download and loading')
+
+    volumeNode = slicer.util.getNode(pattern="FA")
+    logic = find_zFrameLogic()
+    self.assertIsNotNone( logic.hasImageData(volumeNode) )
+    self.delayDisplay('Test passed!')
---
+++ find_zFrame/segmentZframe.py
@@ -0,0 +1,129 @@
+def segment_zFrame(in_img, img_type='MR', withPlots=False):
+    import SimpleITK as sitk
+    import numpy as np
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        withPlots=False
+    import math
+    
+    in_img_np = sitk.GetArrayFromImage(in_img)
+    voxelCount = in_img_np.shape[0]*in_img_np.shape[1]*in_img_np.shape[2]
+
+    hist_y, hist_x=np.histogram(in_img_np.flatten(), bins=int(in_img_np.max()/8))
+    hist_x = hist_x[:-1]
+    cumHist_y = np.cumsum(hist_y.astype(float))/voxelCount
+    #we postulate that the background contain half of the voxels
+    minThreshold_byCount = hist_x[np.where(cumHist_y>0.90)[0][0]]
+    print("background threshold by count found: %d"%minThreshold_byCount)
+
+
+
+    #then we look for minimas on the histogram
+    hist_diff = np.diff(hist_y)
+    hist_diff_zc = np.where(np.diff(np.sign(hist_diff))==2)[0].flatten()
+    if withPlots: print(hist_diff_zc)
+    
+    if img_type == 'MR':
+        minThreshold_byVariation = hist_x[hist_diff_zc[hist_x[hist_diff_zc]>minThreshold_byCount][1]] #use the second minima after background (seems to work ok on t1)
+        print("first maxima after background found: %d"%minThreshold_byVariation)
+    elif img_type == 'CT':
+        secondSpikeVal_intens = hist_x[np.where(hist_y == hist_y[hist_x>minThreshold_byCount].max())]
+        minThreshold_byVariation = hist_x[hist_diff_zc[hist_x[hist_diff_zc]>secondSpikeVal_intens]][0]
+        print("first maxima after soft tissue found: %d"%minThreshold_byVariation)
+    else:
+        print('wrong image type passed !')
+        return 0
+    if withPlots:
+        fig, ax=plt.subplots(2,1, sharex=True)
+        ax[0].plot(hist_x, hist_y, label='intensity histogram')
+        ax[1].plot(hist_x, cumHist_y, label='cumulative histogram')
+        ax[1].axvline(x=minThreshold_byCount, ymin=0.0, ymax=1.0, color='r')
+
+        ax[0].plot(hist_x[:-1], hist_diff, label='derivative of intensity histogram')
+        ax[0].plot(hist_x[hist_diff_zc], hist_diff[hist_diff_zc], linestyle='', marker='o', label="zero crossings of derivative of intensity histogram")
+
+        ax[0].axvline(x=minThreshold_byCount, ymin=0.0, ymax=1.0, color='r')
+        ax[0].axvline(x=minThreshold_byVariation, ymin=0.0, ymax=1.0, color='g')
+        fig.legend()
+        ax[0].grid()
+
+    middleSlice = np.array(np.floor(np.array(in_img_np.shape)/2), dtype=int)
+
+    thresh_img = in_img>minThreshold_byVariation
+
+    if withPlots: 
+        plt.figure()
+        plt.imshow(sitk.GetArrayFromImage(thresh_img)[:,:,middleSlice[2]])
+
+
+    # filter the labels by the size of their bounding boxes
+    spacing_mm = in_img.GetSpacing()
+
+    stats = sitk.LabelShapeStatisticsImageFilter()
+    connectedComponentImage = sitk.ConnectedComponent(thresh_img)
+
+    stats.Execute(connectedComponentImage)
+
+    labelBBox_voxel = np.array([ stats.GetBoundingBox(l) for l in stats.GetLabels()])
+    
+    labelBBox_size_mm = labelBBox_voxel[:,3:]*spacing_mm
+
+
+    #calculate the start and end points of bboxes to detect labels on the edge of the image.
+    labelBBox_startEnd = labelBBox_voxel                                                                                                                          
+    labelBBox_startEnd[:,3:]+=labelBBox_voxel[:,:3]
+
+    labelByBorder_mask=np.logical_not(np.any(labelBBox_startEnd==0, axis=1))
+    labelBySize_mask=np.any(labelBBox_size_mm>100, axis=1)&np.any(labelBBox_size_mm<20, axis=1)
+
+    labelToKeep_mask = np.logical_and(labelByBorder_mask, labelBySize_mask)
+
+    connected_labelMap = sitk.LabelImageToLabelMap(connectedComponentImage)
+
+    label_renameMap = sitk.DoubleDoubleMap()
+    for i, toKeep in enumerate(labelToKeep_mask):
+        if not toKeep:
+            label_renameMap[i+1]=0
+    newLabelMap = sitk.ChangeLabelLabelMap(connected_labelMap, label_renameMap)
+
+    out_img = sitk.BinaryMorphologicalClosing(
+                                        sitk.BinaryErode(
+                                            sitk.BinaryDilate(sitk.LabelMapToLabel(newLabelMap)!=0, 1)
+                                        , 1)
+                                    , (1,1,1), sitk.sitkBall)
+
+    if withPlots:
+        plt.figure()
+        plt.imshow(sitk.GetArrayFromImage(out_img)[:,:,middleSlice[2]]>0, cmap='gray')
+    
+    out_img = sitk.ConnectedComponent(out_img)
+    
+    stats.Execute(out_img)
+
+    print("%d labels were found (id: size):"%(len(stats.GetLabels()) ))
+    for i in stats.GetLabels():
+        print('\t%d: %s'%(i, str(np.array(stats.GetBoundingBox(i)[3:])*spacing_mm)))
+
+    if withPlots: plt.show()
+
+    return out_img
+
+def segment_zFrame_filesystem(in_file, out_file, img_type):
+    import SimpleITK as sitk
+    sitk.WriteImage(segment_zFrame(sitk.ReadImage(in_file), img_type), out_file)
+    
+def segment_zFrame_slicer(inputVolume, outputVolume, img_type):
+    import sitkUtils as siu
+    in_img = siu.PullVolumeFromSlicer(inputVolume)
+    siu.PushVolumeToSlicer(segment_zFrame(in_img, img_type, withPlots=False), outputVolume)
+
+if __name__ == "__main__":
+    import argparse, os
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-i", "--input_image", nargs=1, help="Stereotactic image.", required=True)
+    argParser.add_argument("-o", "--output_image", nargs=1, help="Segmented stereotactic frame.", required=True)
+    argParser.add_argument('-t', '--image_type', nargs=1, help="MR/CT", required=False)
+    args = argParser.parse_args()
+    segment_zFrame_filesystem(os.path.abspath(args.input_image[0]), os.path.abspath(args.output_image[0]), args.image_type[0])
+    
