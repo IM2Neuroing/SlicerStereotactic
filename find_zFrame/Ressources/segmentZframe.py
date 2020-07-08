9fac488c7e3e862bd34047ba0c3abd649e206798
dorian.vogel <dorian.vogel@fhnw.ch>
2020-07-08 09:42:23 +0200

    Improved the frame segmentation algorithm:
    - smooth the histogram before taking the derivative. The bin
    quantization in the histogram resulted in excessive number of
    zero-crossings in the derivative.
    - in order to reject correctly the soft-tissue peak, added an offset of
    100 in intensities from the peak to look for the next minima.
    - only require a 1 to 10 ratio between two of the dimensions of the OBB,
    allows to keep the diagonal of the Z when the two vertical bars are
    missing.

--- find_zFrame/Ressources/segmentZframe.py
+++ find_zFrame/Ressources/segmentZframe.py
@@ -1,6 +1,10 @@
+import pdb
 def segment_zFrame(in_img, img_type='MR', withPlots=False):
     import SimpleITK as sitk
     import numpy as np
+    
+    
+    from scipy.signal import butter, filtfilt
     try:
         import matplotlib.pyplot as plt
     except ImportError:
@@ -10,26 +14,41 @@ def segment_zFrame(in_img, img_type='MR', withPlots=False):
     in_img_np = sitk.GetArrayFromImage(in_img)
     voxelCount = in_img_np.shape[0]*in_img_np.shape[1]*in_img_np.shape[2]
 
-    hist_y, hist_x=np.histogram(in_img_np.flatten(), bins=int(in_img_np.max()/8))
-    hist_x = hist_x[:-1]
+    hist_y, hist_x=np.histogram(in_img_np.flatten(), bins=int(in_img_np.max()/4))
+ 
+    hist_x = hist_x[1:]
+    
+    #filter the histogram
+    
     cumHist_y = np.cumsum(hist_y.astype(float))/voxelCount
     #we postulate that the background contain half of the voxels
     minThreshold_byCount = hist_x[np.where(cumHist_y>0.90)[0][0]]
     print("background threshold by count found: %d"%minThreshold_byCount)
 
 
+    b,a =butter(2, 0.15, btype='low', analog=False)
+    hist_y = filtfilt(b, a, hist_y)
+    
 
     #then we look for minimas on the histogram
     hist_diff = np.diff(hist_y)
+    
+    #b,a =butter(2, 20/(.5*int(in_img_np.max()/8)), btype='low', analog=False)
+    
+    #hist_diff = filtfilt(b, a, hist_diff)
+    
+    
     hist_diff_zc = np.where(np.diff(np.sign(hist_diff))==2)[0].flatten()
-    if withPlots: print(hist_diff_zc)
+    if withPlots: print(hist_x[hist_diff_zc])
     
     if img_type == 'MR':
         minThreshold_byVariation = hist_x[hist_diff_zc[hist_x[hist_diff_zc]>minThreshold_byCount][1]] #use the second minima after background (seems to work ok on t1)
         print("first maxima after background found: %d"%minThreshold_byVariation)
     elif img_type == 'CT':
         secondSpikeVal_intens = hist_x[np.where(hist_y == hist_y[hist_x>minThreshold_byCount].max())]
-        minThreshold_byVariation = hist_x[hist_diff_zc[hist_x[hist_diff_zc]>secondSpikeVal_intens]][0]
+        #minThreshold_byVariation = hist_x[hist_diff_zc[hist_x[hist_diff_zc]>secondSpikeVal_intens]][0]
+        minThreshold_byVariation_id = hist_diff_zc[hist_x[hist_diff_zc] > (minThreshold_byCount+100)][0]
+        minThreshold_byVariation= hist_x[minThreshold_byVariation_id]
         print("first maxima after soft tissue found: %d"%minThreshold_byVariation)
     else:
         print('wrong image type passed !')
@@ -40,7 +59,7 @@ def segment_zFrame(in_img, img_type='MR', withPlots=False):
         ax[1].plot(hist_x, cumHist_y, label='cumulative histogram')
         ax[1].axvline(x=minThreshold_byCount, ymin=0.0, ymax=1.0, color='r')
 
-        ax[0].plot(hist_x[:-1], hist_diff, label='derivative of intensity histogram')
+        ax[0].plot(hist_x[1:], hist_diff, label='derivative of intensity histogram')
         ax[0].plot(hist_x[hist_diff_zc], hist_diff[hist_diff_zc], linestyle='', marker='o', label="zero crossings of derivative of intensity histogram")
 
         ax[0].axvline(x=minThreshold_byCount, ymin=0.0, ymax=1.0, color='r')
@@ -65,13 +84,22 @@ def segment_zFrame(in_img, img_type='MR', withPlots=False):
     connectedComponentImage = sitk.ConnectedComponent(thresh_img)
     
     stats.Execute(connectedComponentImage)
-    labelBBox_voxel = np.array([ stats.GetBoundingBox(l) for l in stats.GetLabels()])
+    
+    print("%d labels were found before filter (id: size):"%(len(stats.GetLabels()) ))
+    for i in stats.GetLabels():
+        print('\t%d: %s'%(i, str(np.array(stats.GetBoundingBox(i)[3:])*spacing_mm)))
+    #labelBBox_voxel = np.array([ stats.GetBoundingBox(l) for l in stats.GetLabels()])
     
     #labelBBox_size_mm = labelBBox_voxel[:,3:]*spacing_mm
     labelBBox_size_mm = np.array([ stats.GetOrientedBoundingBoxSize(l) for l in stats.GetLabels()])
 
-    labelToKeep_mask = np.sum(labelBBox_size_mm>120, axis=1)==2
+    labelToKeep_mask = np.logical_and(np.sum(labelBBox_size_mm>120, axis=1)>0,
+                                      np.sum(np.stack([labelBBox_size_mm[:,0]/labelBBox_size_mm[:,1],
+                                                       labelBBox_size_mm[:,1]/labelBBox_size_mm[:,2],
+                                                       labelBBox_size_mm[:,2]/labelBBox_size_mm[:,0]
+                                                       ], axis=1)>10, axis=1)>0)
     
+    #pdb.set_trace()
     connected_labelMap = sitk.LabelImageToLabelMap(connectedComponentImage)
 
     label_renameMap = sitk.DoubleDoubleMap()
@@ -104,10 +132,16 @@ def segment_zFrame(in_img, img_type='MR', withPlots=False):
 
 def segment_zFrame_filesystem(in_file, out_file, img_type):
     import SimpleITK as sitk
-    sitk.WriteImage(segment_zFrame(sitk.ReadImage(in_file), img_type), out_file)
+    sitk.WriteImage(segment_zFrame(sitk.ReadImage(in_file), img_type, withPlots=True), out_file)
     
 def segment_zFrame_slicer(inputVolume, outputVolume, img_type):
     import sitkUtils as siu
+    try:
+        import scipy
+    except ImportError as e:
+        slicer.util.pip_install('scipy')
+
+
     in_img = siu.PullVolumeFromSlicer(inputVolume)
     siu.PushVolumeToSlicer(segment_zFrame(in_img, img_type, withPlots=False), outputVolume)
 
